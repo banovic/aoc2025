c56fdce613bb0dac38030e2914aeb5f91e1cdf67
Branislav Anovic <branislav.anovic@gmail.com>
2025-12-04 23:19:42 +0100

    Day 1, part 2

--- src/day1.ts
+++ src/day1.ts
@@ -36,10 +36,52 @@ function solution1(filename: string): number {
         }
     }
 
-    console.log('Password:', password)
+    console.log('[1] Password:', password)
 
     return password;
 }
 
+function solution2(filename: string): void {
+    const input = fs.readFileSync(filename, 'utf8');
+
+    const rotations = input
+        .split('\n')
+        .filter(line => line.trim() !== '')
+        .map(parseRotation);
+
+    let x = 50;
+    let password = 0;
+
+    for (const rotation of rotations) {
+        const { direction, distance } = rotation;
+
+        if (direction === 'L') {
+            for (let i = 0; i < distance; i++) {
+                x -= 1;
+                if (x < 0) {
+                    x = 99;
+                }
+                if (x === 0) {
+                    password += 1;
+                }
+            }
+        } else {
+            for (let i = 0; i < distance; i++) {
+                x += 1;
+                if (x > 99) {
+                    x = 0;
+                }
+                if (x === 0) {
+                    password += 1;
+                }
+            }
+        }
+    }
+
+    console.log('[2] Password:', password);
+}
+
 solution1('input/day1-1.txt');
 solution1('input/day1-2.txt');
+solution2('input/day1-1.txt');
+solution2('input/day1-2.txt');
